b84893aab9d2c72fca8913496a1733c33e263ccb
Tyler Kennedy <tkennedy118@gmail.com>
2020-03-19 19:50:18 -0500

    add basic server functionalities

---
+++ server.js
@@ -0,0 +1,28 @@
+/********************************** DEPENDENCIES **********************************/
+const express = require("express");
+
+
+/****************************** EXPRESS CONFIGURATION *****************************/
+
+// tell node to create an 'express' server
+const app = express();
+
+// set initial port
+const PORT = process.env.PORT || 3000
+
+// set Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+/************************************* ROUTER *************************************/
+
+// point to series of route files
+require("./routes/api")(app);
+require("./routes/html")(app);
+
+
+/************************************ LISTENER ************************************/
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+});
